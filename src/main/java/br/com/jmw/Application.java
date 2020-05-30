a418b38759e9e7b6a9f982db32bf2afdfa251c59
Felipe Santaniello <felipe.santaniello-ext@viavarejo.com.br>
2020-05-30 15:06:04 -0300

    Updated graalvm version

--- src/main/java/br/com/jmw/Application.java
+++ src/main/java/br/com/jmw/Application.java
@@ -1,4 +1,29 @@
 package br.com.jmw;
 
-public class Application {
+import picocli.CommandLine;
+import picocli.jansi.graalvm.AnsiConsole;
+
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "jwt", mixinStandardHelpOptions = true,
+        version = "jwt 4.0",
+        description = "Prints the checksum (MD5 by default) of a file to STDOUT.")
+public class Application implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "The file whose checksum to calculate.")
+    private String parameter;
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.printf("Parameter = " + this.parameter);
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode;
+        try (AnsiConsole ansi = AnsiConsole.windowsInstall()) {
+            exitCode = new CommandLine(new Application()).execute(args);
+        }
+        System.exit(exitCode);
+    }
 }
